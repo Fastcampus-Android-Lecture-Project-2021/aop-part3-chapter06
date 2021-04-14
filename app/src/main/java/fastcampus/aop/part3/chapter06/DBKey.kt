9f5bb2fa0c1bc13790acc64c3040c92b0fd5c228
kyeongwan <kang@kyeongwan.kr>
2021-04-15 02:26:30 +0900

    Initial commit part3 chapter06 🎉

---
+++ app/src/main/java/fastcampus/aop/part3/chapter06/DBKey.kt
@@ -0,0 +1,10 @@
+package fastcampus.aop.part3.chapter06
+
+class DBKey {
+    companion object {
+        const val DB_ARTICLES = "Articles"
+        const val DB_USERS = "Users"
+        const val DB_CHATS = "Chats"
+        const val CHILD_CHAT = "chat"
+    }
+}
